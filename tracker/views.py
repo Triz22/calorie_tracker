348ca6b3fce7f03dcf037ced0551150be1adf7d3
Tomi Akin <tomia01@hotmail.com>
2024-09-04 12:51:32 +0000

    add try block to parse json data and handle exception if data is not valid

--- tracker/views.py
+++ tracker/views.py
@@ -9,5 +9,13 @@ def home(request):
         query = request.POST['query']
         api_url = 'https://api.api-ninjas.com/v1/nutrition?query'
         api_request = requests.get(api_url + query, headers ={'X-Api-Key':'yHk5grW8pkVr7MXyZQytZg==RkzMMKohDBMFXg8y'})
+    try:
+        api = json.loads(api_request.content)
+        print(api_request.content)
+    except Exception as e: 
+        api = 'oops, something went wrong try again later!'
+        print(e)
+        return render(request, 'index.html', {'api':api})
+
 
     return render(request, 'index.html')
