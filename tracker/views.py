91a0a3c3877db1bac0b9ff666e04a3355b461fc8
Tomi Akin <tomia01@hotmail.com>
2024-09-04 12:26:21 +0000

    add if method to check form was submited, assign user input to variablee called query, assign api_url variable correct url and construct api request

--- tracker/views.py
+++ tracker/views.py
@@ -5,5 +5,9 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query'
+        api_request = requests.get(api_url + query, headers ={'X-Api-Key':'yHk5grW8pkVr7MXyZQytZg==RkzMMKohDBMFXg8y'})
 
     return render(request, 'index.html')
