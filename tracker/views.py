0ac4c218a7867e1b36df704377ffa3b0d7576800
Tomi Akin <tomia01@hotmail.com>
2024-09-04 12:10:27 +0000

    import json for parsing

--- tracker/views.py
+++ tracker/views.py
@@ -4,12 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     import requests
-    query = '1lb brisket and fries'
-    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-    response = requests.get(api_url, headers={'X-Api-Key': 'yHk5grW8pkVr7MXyZQytZg==RkzMMKohDBMFXg8y'})
-    if response.status_code == requests.codes.ok:
-        print(response.text)
-    else:
-        print("Error:", response.status_code, response.text)
+    import json
 
     return render(request, 'index.html')
