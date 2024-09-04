c6c737c0f38ff391bd71ba0f484b9a90eb1dcdf9
Tomi Akin <tomia01@hotmail.com>
2024-09-04 11:17:22 +0000

    comment out template

--- tracker/views.py
+++ tracker/views.py
@@ -6,7 +6,7 @@ def home(request):
     import requests
     query = '1lb brisket and fries'
     api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-    response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
+    response = requests.get(api_url, headers={'X-Api-Key': 'yHk5grW8pkVr7MXyZQytZg==RkzMMKohDBMFXg8y'})
     if response.status_code == requests.codes.ok:
         print(response.text)
     else:
