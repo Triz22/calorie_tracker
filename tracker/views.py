ec9b3c263e4b69ec315de59dcefd5dc4629acc6f
Tomi Akin <tomia01@hotmail.com>
2024-09-04 09:51:29 +0000

    change api code snippet

--- tracker/views.py
+++ tracker/views.py
@@ -4,13 +4,18 @@ from django.shortcuts import render
 
 def home(request):
     import requests
-    
-    query = '1lb brisket and fries'
-    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-    response = requests.get(api_url, headers={'X-Api-Key': 'yHk5grW8pkVr7MXyZQytZg==RkzMMKohDBMFXg8y'})
-    if response.status_code == requests.codes.ok:
-        print(response.text)
-    else:
-        print("Error:", response.status_code, response.text)
+
+    url = "https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition"
+
+    querystring = {"query":"1lb brisket with fries"}
+
+    headers = {
+	    "x-rapidapi-key": "6506df8921msh4ee0bc6580683d6p15dc39jsn37bad2b71c7c",
+	    "x-rapidapi-host": "nutrition-by-api-ninjas.p.rapidapi.com"
+}
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    print(response.json())
 
     return render(request, 'index.html')
